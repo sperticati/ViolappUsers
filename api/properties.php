c9a4e184548ee2d1f9bedae887ed518012a50809
sperticati@gmail.com <sperticati@gmail.com>
2021-03-09 17:24:46 +0000

    File generated by Skaffolder - user: sperticati@gmail.com

---
+++ api/properties.php
@@ -0,0 +1,10 @@
+<?php 
+	$db_ViolappUsers_db_Url = $_ENV["DB_HOST"] ?? '127.0.0.1';
+	$db_ViolappUsers_db_User = 'root';
+	$db_ViolappUsers_db_Pass = '';
+	$db_ViolappUsers_db_DbName = 'violappusers_db';
+	
+	
+	$jwt_secret_key = 'mySecretKey';
+	
+?>
